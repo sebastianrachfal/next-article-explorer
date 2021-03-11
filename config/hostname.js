fe9b2dfe34e10b03a2f4a545d1c47bad2a624f9b
Sebastian Rachfał <rachfalsebastian@gmail.com>
2021-03-11 18:44:35 +0100

    fetch env fix

--- config/hostname.js
+++ config/hostname.js
@@ -1,2 +1,1 @@
-const hostname = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_HOSTNAME : 'http://localhost:3000/';
-export default hostname;
+export default process.env.NEXT_PUBLIC_HOSTNAME;
